febfb08f0d51538dfe65195b3f5536d0f1b45b1d
Zion203 <jesudas.t@kalvium.community>
2024-05-14 15:32:35 +0530

    commit

---
+++ components/ContactCode.jsx
@@ -0,0 +1,80 @@
+import styles from '../styles/ContactCode.module.css';
+
+const contactItems = [
+  {
+    social: 'website',
+    link: 'jesudaszion.me',
+    href: 'https://nitinranganath.me',
+  },
+  {
+    social: 'email',
+    link: 'jesudaszion@gmail.com',
+    href: 'mailto:jesudaszion@gmail.com',
+  },
+  {
+    social: 'github',
+    link: 'Zion203',
+    href: 'https://github.com/Zion203',
+  },
+  {
+    social: 'linkedin',
+    link: 'Jesudas Zion',
+    href: 'https://www.linkedin.com/in/nitinranganath/',
+  },
+  {
+    social: 'twitter',
+    link: 'jesudas',
+    href: 'https://www.twitter.com/iamnitinr',
+  },
+  {
+    social: 'instagram',
+    link: 'z10n_203',
+    href: 'https://www.instagram.com/iamnitinr',
+  },
+  // {
+  //   social: 'telegram',
+  //   link: 'iamnitinr',
+  //   href: 'https://t.me/iamnitinr',
+  // },
+  // {
+  //   social: 'codepen',
+  //   link: 'nitinranganath',
+  //   href: 'https://codepen.io/itsnitinr',
+  // },
+  // {
+  //   social: 'codesandbox',
+  //   link: 'itsnitinr',
+  //   href: 'https://codesandbox.io/u/itsnitinr',
+  // },
+];
+
+const ContactCode = () => {
+  return (
+    <div className={styles.code}>
+      <p className={styles.line}>
+        <span className={styles.className}>.socials</span> &#123;
+      </p>
+      {contactItems.slice(0, 8).map((item, index) => (
+        <p className={styles.line} key={index}>
+          &nbsp;&nbsp;&nbsp;{item.social}:{' '}
+          <a href={item.href} target="_blank" rel="noopener">
+            {item.link}
+          </a>
+          ;
+        </p>
+      ))}
+      {contactItems.slice(8, contactItems.length).map((item, index) => (
+        <p className={styles.line} key={index}>
+          &nbsp;&nbsp;{item.social}:{' '}
+          <a href={item.href} target="_blank" rel="noopener">
+            {item.link}
+          </a>
+          ;
+        </p>
+      ))}
+      <p className={styles.line}>&#125;</p>
+    </div>
+  );
+};
+
+export default ContactCode;
